0aad5801b611d003a7546fbdc00c30d470a12a82
WrongNicholas <mnwojnar2000@gmail.com>
2024-05-22 23:00:07 -0400

    Change typename 'T' to 'Value'

--- linear/LinkedList.hpp
+++ linear/LinkedList.hpp
@@ -6,69 +6,69 @@
 #include <iostream>
 
 // Struct defining a node in linked list
-template <typename T>
+template <typename Value>
 struct ListNode {
-  T value;      // Value stored in the node
+  Value value;      // Value stored in the node
   ListNode* next;   // Pointer to the next node in the list
   
   // Constructor to initialize the node with a value
-  ListNode(T value) : value(value), next(nullptr) {}
+  ListNode(Value value) : value(value), next(nullptr) {}
 };
 
 // Class representing singly linked list
-template <typename T>
+template <typename Value>
 class LinkedList {
 private:
-  ListNode<T>* head;    // Pointer to the first node in the list
-  ListNode<T>* tail;    // Pointer to the last node in the list
+  ListNode<Value>* head;    // Pointer to the first node in the list
+  ListNode<Value>* tail;    // Pointer to the last node in the list
 
   // Private helper function to get node at a specific index
-  ListNode<T>* get_node_at(int index) const;
+  ListNode<Value>* get_node_at(int index) const;
 
 public:                                    
   // Constructors and Destructor           
   LinkedList() : head(nullptr), tail(nullptr) {}            // Default constructor
-  LinkedList(const LinkedList<T> &other);                   // Copy constructor
+  LinkedList(const LinkedList<Value> &other);                   // Copy constructor
   ~LinkedList();                                            // Destructor
    
   // Accessors
-  T& operator[](int index);                                 // Overloaded subscript operator
-  const T& operator[](int index) const;                     // Overloaded const subscript operator
-  T& front();                                               // Returns value at the front of the list
-  const T& front() const;                                   // Returns value at the front of the list (const)
-  T& back();                                                // Returns value at the back of the list
-  const T& back() const;                                    // Returns value at the back of the list (const)
+  Value& operator[](int index);                                 // Overloaded subscript operator
+  const Value& operator[](int index) const;                     // Overloaded const subscript operator
+  Value& front();                                               // Returns value at the front of the list
+  const Value& front() const;                                   // Returns value at the front of the list (const)
+  Value& back();                                                // Returns value at the back of the list
+  const Value& back() const;                                    // Returns value at the back of the list (const)
   int size() const;                                         // Returns the number of elements in the list
   bool empty() const;                                       // Checks if the list empty
 
   // Mutators
-  void push_front(const T& value);                          // Adds a new element at the front of the list
-  void push_back(const T& value);                           // Adds a new element at the back of the list
-  void insert(int index, const T& value);                   // Adds a new element at the specified index
-  void remove_all(const T& value);                          // Removes all occurances of a value from the list
+  void push_front(const Value& value);                          // Adds a new element at the front of the list
+  void push_back(const Value& value);                           // Adds a new element at the back of the list
+  void insert(int index, const Value& value);                   // Adds a new element at the specified index
+  void remove_all(const Value& value);                          // Removes all occurances of a value from the list
   void remove_at(int index);                                // Removes the element at specified index
   void pop_front();                                         // Removes the element at the front of the list
   void pop_back();                                          // Removes the element at the back of the list
   void clear();                                             // Removes all elements from the list
 
   // Utility
-  int find(const T& value) const;                           // Finds the index of the first occurance of a value
-  bool contains(const T& value) const;                      // Checks if the list contains a specific value
+  int find(const Value& value) const;                           // Finds the index of the first occurance of a value
+  bool contains(const Value& value) const;                      // Checks if the list contains a specific value
   void print();                                             // Prints all elements in the list
 
   // Iterator
   class Iterator {
   private:
-    ListNode<T>* current;   // Pointer to the current node in the iteration
+    ListNode<Value>* current;   // Pointer to the current node in the iteration
   public:
     // Constructor
-    Iterator(ListNode<T>* node) : current(node) { }
+    Iterator(ListNode<Value>* node) : current(node) { }
 
     // Dereference operator
-    T& operator*() const { return current->value; }
+    Value& operator*() const { return current->value; }
 
     // Get the current node
-    ListNode<T>* get_node() { return current; }
+    ListNode<Value>* get_node() { return current; }
 
     // Increment operator
     Iterator& operator++() { current = current->next; return *this; }
@@ -84,8 +84,8 @@ public:
   Iterator end() const { return Iterator(nullptr); }  // Returns a const iterator pointing to the end (nullptr)
 };
 
-template <typename T>
-ListNode<T>* LinkedList<T>::get_node_at(int index) const {
+template <typename Value>
+ListNode<Value>* LinkedList<Value>::get_node_at(int index) const {
   if (index < 0) {
     throw std::out_of_range("Index out of range");
   }
@@ -100,18 +100,18 @@ ListNode<T>* LinkedList<T>::get_node_at(int index) const {
   throw std::out_of_range("Index out of range");
 }
 
-template <typename T>
-LinkedList<T>::LinkedList(const LinkedList& other) : head(nullptr), tail(nullptr) {
+template <typename Value>
+LinkedList<Value>::LinkedList(const LinkedList& other) : head(nullptr), tail(nullptr) {
   if (other.head == nullptr) {
     return;
   }
   
-  this->head = new ListNode<T>(other.head->value);
-  ListNode<T>* current = this->head;
-  ListNode<T>* temp = other.head->next;
+  this->head = new ListNode<Value>(other.head->value);
+  ListNode<Value>* current = this->head;
+  ListNode<Value>* temp = other.head->next;
 
   while (temp != nullptr) {
-    current->next = new ListNode<T>(temp->value);
+    current->next = new ListNode<Value>(temp->value);
     current = current->next;
     temp = temp->next;
   }
@@ -119,23 +119,23 @@ LinkedList<T>::LinkedList(const LinkedList& other) : head(nullptr), tail(nullptr
   this->tail = current;
 }
 
-template <typename T>
-LinkedList<T>::~LinkedList() {
+template <typename Value>
+LinkedList<Value>::~LinkedList() {
   clear();
 } 
 
-template <typename T>
-T& LinkedList<T>::operator[](int index) {
+template <typename Value>
+Value& LinkedList<Value>::operator[](int index) {
   return get_node_at(index)->value;
 }
 
-template <typename T>
-const T& LinkedList<T>::operator[](int index) const {
+template <typename Value>
+const Value& LinkedList<Value>::operator[](int index) const {
   return get_node_at(index)->value;
 }
 
-template <typename T>
-T& LinkedList<T>::front() {
+template <typename Value>
+Value& LinkedList<Value>::front() {
   if (this->head == nullptr) {
     throw std::out_of_range("List is empty");
   }
@@ -143,8 +143,8 @@ T& LinkedList<T>::front() {
   return this->head->value;
 }
 
-template <typename T>
-const T& LinkedList<T>::front() const {
+template <typename Value>
+const Value& LinkedList<Value>::front() const {
   if (this->head == nullptr) {
     throw std::out_of_range("List is empty");
   }
@@ -152,8 +152,8 @@ const T& LinkedList<T>::front() const {
   return this->head->value;
 }
 
-template <typename T>
-T& LinkedList<T>::back() {
+template <typename Value>
+Value& LinkedList<Value>::back() {
   if (this->tail == nullptr) {
     throw std::out_of_range("List is empty");
   }
@@ -161,8 +161,8 @@ T& LinkedList<T>::back() {
   return this->tail->value;
 }
 
-template <typename T>
-const T& LinkedList<T>::back() const {
+template <typename Value>
+const Value& LinkedList<Value>::back() const {
   if (this->tail == nullptr) {
     throw std::out_of_range("List is empty");
   }
@@ -170,21 +170,21 @@ const T& LinkedList<T>::back() const {
   return this->tail->value;
 }
 
-template <typename T>
-int LinkedList<T>::size() const {
+template <typename Value>
+int LinkedList<Value>::size() const {
   int count = 0;
   for (auto element = begin(); element != end(); ++element, ++count);
   return count;
 }
 
-template <typename T>
-bool LinkedList<T>::empty() const {
+template <typename Value>
+bool LinkedList<Value>::empty() const {
   return this->head == nullptr;
 }
 
-template <typename T>
-void LinkedList<T>::push_front(const T& value) {
-  ListNode<T>* node = new ListNode<T>(value);
+template <typename Value>
+void LinkedList<Value>::push_front(const Value& value) {
+  ListNode<Value>* node = new ListNode<Value>(value);
   
   if (this->head == nullptr) {
     this->head = node;
@@ -196,9 +196,9 @@ void LinkedList<T>::push_front(const T& value) {
   this->head = node;
 }
 
-template <typename T>
-void LinkedList<T>::push_back(const T& value) {
-  ListNode<T>* node = new ListNode<T>(value);
+template <typename Value>
+void LinkedList<Value>::push_back(const Value& value) {
+  ListNode<Value>* node = new ListNode<Value>(value);
   
   if (this->head == nullptr) {
     this->head = node;
@@ -210,8 +210,8 @@ void LinkedList<T>::push_back(const T& value) {
   this->tail = node;
 }
 
-template <typename T>
-void LinkedList<T>::insert(int index, const T& value) {
+template <typename Value>
+void LinkedList<Value>::insert(int index, const Value& value) {
   if (index < 0) {
     throw std::out_of_range("Index out of range");
   }
@@ -221,9 +221,9 @@ void LinkedList<T>::insert(int index, const T& value) {
     return;
   }
 
-  ListNode<T>* current = get_node_at(index - 1);
+  ListNode<Value>* current = get_node_at(index - 1);
   
-  ListNode<T>* node = new ListNode<T>(value);
+  ListNode<Value>* node = new ListNode<Value>(value);
 
   node->next = current->next;
   current->next = node;
@@ -233,10 +233,10 @@ void LinkedList<T>::insert(int index, const T& value) {
   }
 }
 
-template <typename T>
-void LinkedList<T>::remove_all(const T& value) {
-  ListNode<T>* current = this->head;
-  ListNode<T>* previous = nullptr;
+template <typename Value>
+void LinkedList<Value>::remove_all(const Value& value) {
+  ListNode<Value>* current = this->head;
+  ListNode<Value>* previous = nullptr;
 
   while (current != nullptr) {
     if (current->value == value) {
@@ -251,7 +251,7 @@ void LinkedList<T>::remove_all(const T& value) {
       if (previous != nullptr) {
         previous->next = current->next;
       } 
-      ListNode<T>* temp = current;
+      ListNode<Value>* temp = current;
       current = current->next;
       delete temp;
     }
@@ -262,8 +262,8 @@ void LinkedList<T>::remove_all(const T& value) {
   }
 }
 
-template <typename T>
-void LinkedList<T>::remove_at(int index) {
+template <typename Value>
+void LinkedList<Value>::remove_at(int index) {
   if (index < 0 || index >= this->size()) {
     throw std::out_of_range("Index out of range");
   }
@@ -273,9 +273,9 @@ void LinkedList<T>::remove_at(int index) {
     return;
   }
 
-  ListNode<T>* previous = get_node_at(index - 1);
+  ListNode<Value>* previous = get_node_at(index - 1);
 
-  ListNode<T>* temp = previous->next;
+  ListNode<Value>* temp = previous->next;
   previous->next = temp->next;
 
   if (temp == tail) {
@@ -285,13 +285,13 @@ void LinkedList<T>::remove_at(int index) {
   delete temp;
 }
 
-template <typename T>
-void LinkedList<T>::pop_front() {
+template <typename Value>
+void LinkedList<Value>::pop_front() {
   if (this->head == nullptr) {
     return;
   }
   
-  ListNode<T>* temp = this->head;
+  ListNode<Value>* temp = this->head;
   this->head = this->head->next;
   
   if (this->head == nullptr) {
@@ -301,8 +301,8 @@ void LinkedList<T>::pop_front() {
   delete temp;
 }
 
-template <typename T>
-void LinkedList<T>::pop_back() {
+template <typename Value>
+void LinkedList<Value>::pop_back() {
   if (this->head == nullptr) {
     throw std::out_of_range("List is empty");
   }
@@ -314,7 +314,7 @@ void LinkedList<T>::pop_back() {
     return;
   }
   
-  ListNode<T>* current = this->head;
+  ListNode<Value>* current = this->head;
   while (current->next != this->tail) {
     current = current->next;
   }
@@ -324,11 +324,11 @@ void LinkedList<T>::pop_back() {
   this->tail->next = nullptr;
 }
 
-template <typename T>
-void LinkedList<T>::clear() {
-  ListNode<T>* current = this->head;
+template <typename Value>
+void LinkedList<Value>::clear() {
+  ListNode<Value>* current = this->head;
   while (current != nullptr) {
-    ListNode<T>* next = current->next;
+    ListNode<Value>* next = current->next;
     delete current;
     current = next;
   }
@@ -337,8 +337,8 @@ void LinkedList<T>::clear() {
   this->tail = nullptr;
 }
 
-template <typename T>
-int LinkedList<T>::find(const T& value) const {
+template <typename Value>
+int LinkedList<Value>::find(const Value& value) const {
   int count = 0;
   for (auto element = begin(); element != end(); ++element, ++count) {
     if (*element == value) {
@@ -349,8 +349,8 @@ int LinkedList<T>::find(const T& value) const {
   throw std::runtime_error("Value not found");
 }
 
-template <typename T>
-bool LinkedList<T>::contains(const T& value) const {
+template <typename Value>
+bool LinkedList<Value>::contains(const Value& value) const {
 
   for (auto element = begin(); element != end(); ++element) {
     if (*element == value) {
@@ -361,8 +361,8 @@ bool LinkedList<T>::contains(const T& value) const {
   return false;
 }
 
-template <typename T>
-void LinkedList<T>::print() {
+template <typename Value>
+void LinkedList<Value>::print() {
   if (this->head == nullptr) {
     std::cout << "Ø" << std::endl;
     return;
